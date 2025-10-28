4d7c23ab5137c91b4f15e010eb880112785cbdb4
Alexandr Culka <alexandr.culka@aura.cz>
2025-10-28 12:19:05 +0100

    feat: object literal annoataions

--- annotations/variables.js
+++ annotations/variables.js
@@ -6,3 +6,19 @@ var nothingMuch = null;
 var nothing = undefined;
 // built in objects
 var now = new Date();
+// Array
+var colors = ["red", "green", "blue"];
+var myNumbers = [1, 2, 3];
+var truths = [true, true, false];
+// Classes
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    return Car;
+}());
+var car = new Car();
+// Object literal
+var point = {
+    x: 10,
+    y: 20,
+};
--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,5 +1,5 @@
 const apples: number = 5;
-let speed:string = "fast";
+let speed: string = "fast";
 
 let nothingMuch: null = null;
 let nothing: undefined = undefined;
@@ -7,5 +7,21 @@ let nothing: undefined = undefined;
 // built in objects
 let now: Date = new Date();
 
-export {};
+// Array
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
 
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+
+
+export {};
