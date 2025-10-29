7e4cba324a3324669978604ab4544fb89e014ea2
Alexandr Culka <alexandr.culka@aura.cz>
2025-10-29 20:11:19 +0100

    feat: typed arrays

--- arrays.ts
+++ arrays.ts
@@ -11,4 +11,23 @@ const dates = [new Date(), new Date()];
 // const carsByMake: string[][]
 const carsByMake = [["f140"], ["corolla"], ["camaro"]];
 
+// Help with inference when extracting values - no annotation needed when declaring
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+// carMakers.push(100); // Error
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+}
+);
+
+// Flexible types
+const importantDates: (Date | string)[] = [];
+importantDates.push("2030-10-10");
+importantDates.push(new Date());
+// importantDates.push(100); // Error
+
 export {};
