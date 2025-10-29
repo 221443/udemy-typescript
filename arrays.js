5cbc5c8c86b9d85bbd9befddcb49c96a9e24d9e2
Alexandr Culka <alexandr.culka@aura.cz>
2025-10-29 18:14:33 +0100

    feat: arrays, multidimensional arrays

---
+++ arrays.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var carMakers = ["ford", "toyota", "chevy"];
+var carMakersAnnotated = ["ford", "toyota", "chevy"];
+// const arrayAny: any[]
+var arrayAny = [];
+// const dates: Date[]
+var dates = [new Date(), new Date()];
+// const carsByMake: string[][]
+var carsByMake = [["f140"], ["corolla"], ["camaro"]];
---
+++ arrays.ts
@@ -0,0 +1,14 @@
+const carMakers = ["ford", "toyota", "chevy"];
+
+const carMakersAnnotated: string[] = ["ford", "toyota", "chevy"];
+
+// const arrayAny: any[]
+const arrayAny = [];
+
+// const dates: Date[]
+const dates = [new Date(), new Date()];
+
+// const carsByMake: string[][]
+const carsByMake = [["f140"], ["corolla"], ["camaro"]];
+
+export {};
