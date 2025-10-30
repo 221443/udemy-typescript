569f113492a19e977a5f771403fe668615299595
Alexandr Culka <alexandr.culka@aura.cz>
2025-10-30 11:21:32 +0100

    feat: interfaces can be used even whem the object have more properties than is in interface

--- interfacesAndAddititonalProperties.js
+++ interfacesAndAddititonalProperties.js
@@ -9,8 +9,29 @@ var oldCivic = {
         return "\n    Name: ".concat(this.name, "\n    Year: ").concat(this.year, "\n    Date of Manufacture: ").concat(this.dateOfManufacture, "\n    Broken: ").concat(this.broken);
     },
 };
-// The function have to use only the properties defined in the interface. 
-var printVehicle2 = function (vehicle) {
+// The function have to use only the properties defined in the interface.
+var printVehicle = function (vehicle) {
     console.log("Summary: ".concat(vehicle.summary()));
 };
-printVehicle2(oldCivic);
+printVehicle(oldCivic);
+var oldCivic2 = {
+    name: "civic",
+    year: 2000,
+    broken: true,
+    summary: function () {
+        return "Name: ".concat(this.name, ", Year: ").concat(this.year, ", Broken: ").concat(this.broken);
+    },
+};
+var drink = {
+    color: "brown",
+    carbonated: true,
+    sugar: 40,
+    summary: function () {
+        return "Color: ".concat(this.color, ", Carbonated: ").concat(this.carbonated, ", Sugar: ").concat(this.sugar);
+    },
+};
+var printSummary2 = function (item) {
+    console.log(item.summary());
+};
+printSummary2(oldCivic2);
+printSummary2(drink);
--- interfacesAndAddititonalProperties.ts
+++ interfacesAndAddititonalProperties.ts
@@ -18,11 +18,39 @@ const oldCivic = {
   },
 };
 
-// The function have to use only the properties defined in the interface. 
-const printVehicle2 = (vehicle: Vehicle) => {
+// The function have to use only the properties defined in the interface.
+const printVehicle = (vehicle: Vehicle) => {
   console.log(`Summary: ${vehicle.summary()}`);
 };
-printVehicle2(oldCivic);
+printVehicle(oldCivic);
 
+// Possible use of more generic interface
+interface Reportable {
+  summary(): string;
+}
+
+const oldCivic2 = {
+  name: "civic",
+  year: 2000,
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}, Year: ${this.year}, Broken: ${this.broken}`;
+  },
+};
+
+const drink = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `Color: ${this.color}, Carbonated: ${this.carbonated}, Sugar: ${this.sugar}`;
+  },
+};
+
+const printSummary2 = (item: Reportable): void => {
+  console.log(item.summary());
+};
+printSummary2(oldCivic2);
+printSummary2(drink);
 
 export {};
